a135f4e35b789b8901d193023afc4c4d2c5a2c06
Olasubomi Odekunle <subomi.odekunle@flutterwavego.com>
2024-02-06 23:21:16 +0100

    Initial commit

---
+++ src/main/java/com/example/websocketsspring/WebsocketsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websocketsspring/WebsocketsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
